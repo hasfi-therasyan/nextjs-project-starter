0af589085795ed26819e3be23a77436489853836
hasfi-therasyan <96185871+hasfi-therasyan@users.noreply.github.com>
2025-05-17 20:22:59 +0700

    Add build.gradle.kts 1747488175015

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.apollographql.apollo3:apollo-gradle-plugin:3.8.2")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
